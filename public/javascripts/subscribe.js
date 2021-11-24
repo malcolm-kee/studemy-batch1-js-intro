5b43d18be3fe3fbe5b23d952856113a986e66808
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-11-24 19:29:53 +0800

    complete do it slide-176

--- public/javascripts/subscribe.js
+++ public/javascripts/subscribe.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 
 function showNotification() {
-    const notification = document.querySelector('#signup-notification');
-    // remove 'hidden' class on #signup-notification
-    notification.classList.remove('hidden');
-    timerId = setTimeout(() => {
-        // add the 'hidden' class back after 2 second
-        notification.classList.add('hidden');
-    }, 2000);
+  const notification = document.querySelector('#signup-notification');
+  // remove 'hidden' class on #signup-notification
+  notification.classList.remove('hidden');
+  timerId = setTimeout(() => {
+    // add the 'hidden' class back after 2 second
+    notification.classList.add('hidden');
+  }, 2000);
 }
 
 const form = document.querySelector('#subscribe-form');
@@ -16,22 +16,22 @@ const emailInput = document.querySelector('#email-address');
 let isSubmitting = false;
 
 form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
+  ev.preventDefault();
 
-    if (!isSubmitting) {
-        isSubmitting = true;
+  if (!isSubmitting) {
+    isSubmitting = true;
 
-        helpers.signupEmail({
-            email: emailInput.value,
-            onSuccess: (res) => {
-                console.log('signup email success');
-                console.log(res)
-                isSubmitting = false;
-            },
-            onError: () => {
-                console.log('signup email error');
-            }
-        });
+    helpers.signupEmail({
+      email: emailInput.value,
+      onSuccess: (res) => {
+        console.log('signup email success');
+        console.log(res);
+        isSubmitting = false;
         showNotification();
-    }
+      },
+      onError: () => {
+        console.log('signup email error');
+      }
+    });
+  }
 });
