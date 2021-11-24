cf0f0e8d86558ebccac6f38b8710a271edc6231f
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2021-11-25 01:12:02 +0800

    doit slide 198

--- public/javascripts/q-and-a.js
+++ public/javascripts/q-and-a.js
@@ -3,29 +3,29 @@
 (function initGuide() {
   const questionDemoBtn = document.querySelector('#question-demo-btn');
 
+  function prompt(options) {
+    return new Promise((resolve) => {
+      helpers.prompt(options, (ans) => resolve(ans));
+    });
+  }
+
   questionDemoBtn.addEventListener('click', () => {
-    helpers.prompt(
-      {
-        type: 'question',
-        label: 'What is your favourite animal?'
-      },
-      (answer) => {
-        console.log('Answer is ', answer);
-      }
-    );
+    prompt({
+      type: 'question',
+      label: 'What is your favourite animal?'
+    }).then((answer) => {
+      console.log('Answer is ', answer);
+    });
   });
 
   const confirmationDemoBtn = document.querySelector('#confirmation-demo-btn');
   confirmationDemoBtn.addEventListener('click', () => {
-    helpers.prompt(
-      {
-        type: 'confirmation',
-        label: 'Are you sure you want to proceed?'
-      },
-      (answer) => {
-        console.log('Answer is', answer);
-      }
-    );
+    prompt({
+      type: 'confirmation',
+      label: 'Are you sure you want to proceed?'
+    }).then((answer) => {
+      console.log('Answer is', answer);
+    });
   });
 
   // TODO: write code so that when user click "Age" button, prompt them with the
@@ -33,15 +33,12 @@
   // E.g. when user enter 21, you should log "The age is 21."
   const askAgeBtn = document.querySelector('#ask-age-btn');
   askAgeBtn.addEventListener('click', () => {
-    helpers.prompt(
-      {
-        type: 'question',
-        label: 'What is your age?'
-      },
-      (answer) => {
-        console.log('The age is ', answer);
-      }
-    );
+    prompt({
+      type: 'question',
+      label: 'What is your age?'
+    }).then((answer) => {
+      console.log('The age is ', answer);
+    });
   });
 
   // TODO: write code so that when user click "NSFW Content" button, prompt them with
@@ -61,15 +58,12 @@
       },
       (toProceed) => {
         if (toProceed) {
-          helpers.prompt(
-            {
-              type: 'question',
-              label: 'What is your age?'
-            },
-            (providedAge) => {
-              console.log('User choose to proceed with age ', providedAge);
-            }
-          );
+          prompt({
+            type: 'question',
+            label: 'What is your age?'
+          }).then((providedAge) => {
+            console.log('User choose to proceed with age ', providedAge);
+          });
         } else {
           console.log('User choose to not proceed.');
         }
